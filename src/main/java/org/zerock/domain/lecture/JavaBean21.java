03984ee082479bb4b0731d3a84282b3ecf8ad94d
jejwon <jeje5073@gmail.com>
2022-11-03 14:41:58 +0900

    11/03 half

---
+++ src/main/java/org/zerock/domain/lecture/JavaBean21.java
@@ -0,0 +1,13 @@
+package org.zerock.domain.lecture;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class JavaBean21 {
+	private String name;
+	private int age;
+	private boolean hasCar;
+	private List<String> food;
+}
