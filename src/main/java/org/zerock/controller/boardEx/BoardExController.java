8efd338ae402bf611dd89177dadb78cc960a1d7f
jejwon <jeje5073@gmail.com>
2022-11-06 22:02:57 +0900

    board prac

--- src/main/java/org/zerock/controller/boardEx/BoardExController.java
+++ src/main/java/org/zerock/controller/boardEx/BoardExController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domainEx.BoardEx.BoardDtoEx;
 import org.zerock.serviceEx.boardEx.BoardServiceEx;
 
@@ -22,11 +23,20 @@ public class BoardExController {
 	}
 	
 	@PostMapping("registerEx")
-	public String register(BoardDtoEx board) {
+	public String register(BoardDtoEx board, RedirectAttributes rttr) { //session에 담아도 됨
 		//request param 수집, 가공
 		
 		//business logic
 		int cnt = service.register(board); //의존성 주입
+		
+		//modal message -> redirect 할 때 데이터 넘겨주기
+		if(cnt == 1) {
+			rttr.addFlashAttribute("message", "새 게시물이 등록되었습니다.");
+		} else {
+			rttr.addFlashAttribute("message", "새 게시물이 등록되지 않았습니다.");
+		}
+		
+		return "redirect:/boardEx/listEx";
 	}
 	
 }
