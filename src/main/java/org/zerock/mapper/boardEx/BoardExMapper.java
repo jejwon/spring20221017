70d29985bf63e5f5adc8a65a688495aa2716e614
jejwon <jeje5073@gmail.com>
2022-11-07 17:48:20 +0900

    11/7

--- src/main/java/org/zerock/mapper/boardEx/BoardExMapper.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.mapper.boardEx;
-
-import org.zerock.domainEx.BoardEx.BoardDtoEx;
-
-public interface BoardExMapper {
-
-	int insert(BoardDtoEx board);
-	
-}
--- src/main/java/org/zerock/mapper/lecture/Mapper11.java
+++ src/main/java/org/zerock/mapper/lecture/Mapper11.java
@@ -1,5 +1,8 @@
 package org.zerock.mapper.lecture;
 
+import java.util.List;
+
+
 import org.zerock.domain.lecture.JavaBean18;
 import org.zerock.domain.lecture.JavaBean19;
 
@@ -7,16 +10,23 @@ public interface Mapper11 {
 	int updateCustomer(JavaBean18 customer);
 
 	JavaBean18 getCustomerById(int id);
-	
+
+	JavaBean19 getSupplierById(Integer id);
+
 	int updateSupplier(JavaBean19 supplier);
-	
-	JavaBean19 getSupplierById(int id);
-	
+
 	int updateBank(int account, int money);
 
 	int insertCustomer(JavaBean18 customer);
 
 	int insertSupplier(JavaBean19 supplier);
 
+
 	int deleteCustomer(int id);
+
+	int deleteSupplier(int id);
+
+	List<JavaBean18> getCustomers();
+
+	List<JavaBean19> getSuppliers();
 }
